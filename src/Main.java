a118ee1a15ffa32a99f85d1c348192c50fa4963a
Andrea <andreaeva.lengyel@gmail.com>
2019-10-08 23:16:28 +0300

    Assignment

--- src/Main.java
+++ src/Main.java
@@ -22,15 +22,25 @@ public class Main {
         System.out.println(sum(A,B));
 
         float C = division(A,B);
-        System.out.println(C);
+            System.out.println(C);
+            System.out.println(Op1(-5,8,6));
+            System.out.println(Op2(55, 9, 9));
+            System.out.println(Op3(20, -3, 5, 8));
+            System.out.println(Op4(5, 15, 3, 2, 8));
+//        Exercise 1
 
-        System.out.println(Op1(-5,8,6));
+        int x = 3;
+        int y = 5;
+        int result = sum(x,y);
+        System.out.println("The result of sum is: " + result);
+        result = substract(x,y);
+        System.out.println("The result of substract is: " + result);
+        float result2 = division(x,y);
+        System.out.println("The result of division is: " + result2);
+        result = multiply(x,y);
+        System.out.println("The result of multiplication is: " + result);
+//        Exercise 2
 
-        System.out.println(Op2(55, 9, 9));
-
-        System.out.println(Op3(20, -3, 5, 8));
-
-        System.out.println(Op4(5, 15, 3, 2, 8));
     }
     public static void printMyName(){
         System.out.println("Hello \nAndrea");
@@ -58,5 +68,14 @@ public class Main {
     public static int Op4(int a, int b, int c, int d, int e){
         int rez = a + b/c * d - e % c;
         return rez;
+    }
+
+        public static int substract(int first, int second) {
+        int rez = first - second;
+        return rez;
+    }
+        public static int multiply(int first, int second) {
+        int mult = first * second;
+        return mult;
     }
 }
